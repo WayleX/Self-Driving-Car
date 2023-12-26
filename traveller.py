1bc7ff84f507bdc502bcaca66093be8b622fe01c
Mykola <vysotskyi.pn@ucu.edu.ua>
2023-12-26 13:06:28 +0200

    new best

--- traveller.py
+++ traveller.py
@@ -15,7 +15,7 @@ timeout = 0.0001
 
 def decide(arduino, sensors_data, state):
     if state == "F":
-        if sensors_data["F"] > 200:
+        if sensors_data["F"] > 100:
             pass
         elif sensors_data["R"] > sensors_data["L"]:
             car_controller.send_command(arduino, 'L'*60)
@@ -24,13 +24,13 @@ def decide(arduino, sensors_data, state):
             car_controller.send_command(arduino, 'R'*60)
             state = "L"
     if state == "R":
-        if sensors_data["F"] > 200:
+        if sensors_data["F"] > 100:
             car_controller.send_command(arduino, 'R'*60)
             state = "F"
         else:
             pass
     if state == "L":
-        if sensors_data["F"] > 200:
+        if sensors_data["F"] > 100:
             car_controller.send_command(arduino, 'L'*60)
             state = "F"
         else:
